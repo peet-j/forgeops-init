daa02648e950a9591225bc558e065943df667d4a
Chandra <chandra.dhulipala@amido.com>
2017-11-17 14:43:34 +0000

    AM customization script and its supporting(custom authN module jar) file

---
+++ default/am/empty-import/customize-am.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# This is a placeholder that you can replace with your own script to copy in assets such as images
+# or a web.xml file.
+# Some Environment variables that are available:
+# CATALINA_HOME: The expanded openam war file is in $CATALINA_HOME/webapps/openam
+# OPENAM_HOME
+# DOMAIN: The cookie domain (includes leading .)
+# NAMESPACE: The Kubernetes namespace
+echo "AM customization script"
+
+echo "Customizing the AM web application"
+echo ""
+
+echo "Available environment variables:"
+echo ""
+env
+echo ""
+
+# Copy the custom authN module ZeroPageUsernameSharedNode WAR file that is in the same directory as this script to the
+# webapps/openam/WEB-INF/lib directory
+cp zeropageusernameshared-node-5.5.0.jar ${CATALINA_HOME}/webapps/openam/WEB-INF/lib
+
+echo "Finished copying the custom authentication module jar file"
+echo ""
+echo "Finished customizing the AM web application"
+
+
