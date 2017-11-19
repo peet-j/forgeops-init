23678e2bce4029183e645bf9126425856aa78765
Chandra <chandra.dhulipala@amido.com>
2017-11-19 02:08:54 +0000

    AM customization script update for copying jar file

--- default/am/cd-dev/customize-am.sh
+++ default/am/cd-dev/customize-am.sh
@@ -18,7 +18,7 @@ echo ""
 
 # Copy the custom authN module ZeroPageUsernameSharedNode WAR file that is in the same directory as this script to the
 # webapps/openam/WEB-INF/lib directory
-cp zeropageusernameshared-node-5.5.0.jar ${CATALINA_HOME}/webapps/openam/WEB-INF/lib
+cp /git/forgeops-init/default/am/cd-dev/zeropageusernameshared-node-5.5.0.jar ${CATALINA_HOME}/webapps/openam/WEB-INF/lib
 
 echo "Finished copying the custom authentication module jar file"
 echo ""
